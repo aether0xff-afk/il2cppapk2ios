4d3bb19bcf27446c71397de4b7fb53918ba9c81f
aether0xff-afk <aether0xff@gmail.com>
2026-09-21 15:00:43 +0900

    Show IL2CPP probe progress on device

--- ios_host/main.m
+++ ios_host/main.m
@@ -5,14 +5,24 @@
 typedef void (*il2cpp_init_fn)(const char *);
 typedef void (*il2cpp_shutdown_fn)(void);
 
+static UILabel *gStatusLabel;
+
+static void a2i_status(NSString *message) {
+    NSLog(@"[a2i] %@", message);
+    dispatch_async(dispatch_get_main_queue(), ^{
+        gStatusLabel.text = [NSString stringWithFormat:@"%@\n\n%@", gStatusLabel.text ?: @"", message];
+    });
+}
+
 static void a2i_log_dlerror(NSString *stage) {
     const char *err = dlerror();
-    NSLog(@"[a2i] %@: %s", stage, err ? err : "(no dlerror)");
+    NSString *message = [NSString stringWithFormat:@"%@: %s", stage, err ? err : "(no dlerror)"];
+    a2i_status(message);
 }
 
 static void a2i_probe(void) {
     @autoreleasepool {
-        NSLog(@"[a2i] iOS host started");
+        a2i_status(@"iOS host started");
         NSBundle *bundle = NSBundle.mainBundle;
         NSString *shimPath = [bundle pathForResource:@"libbionic_shim" ofType:@"dylib"];
         NSString *il2cppPath = [bundle pathForResource:@"libil2cpp_ported" ofType:@"dylib"];
@@ -23,31 +33,31 @@ static void a2i_probe(void) {
         dlerror();
         void *shim = dlopen(shimPath.fileSystemRepresentation, RTLD_NOW | RTLD_GLOBAL);
         if (!shim) { a2i_log_dlerror(@"dlopen shim failed"); return; }
-        NSLog(@"[a2i] shim loaded");
+        a2i_status(@"shim loaded");
         dlerror();
         void *il2cpp = dlopen(il2cppPath.fileSystemRepresentation, RTLD_NOW | RTLD_GLOBAL);
         if (!il2cpp) { a2i_log_dlerror(@"dlopen translated libil2cpp failed"); return; }
-        NSLog(@"[a2i] translated libil2cpp loaded");
+        a2i_status(@"translated libil2cpp loaded");
         dlerror();
         il2cpp_init_fn il2cpp_init = (il2cpp_init_fn)dlsym(il2cpp, "il2cpp_init");
         if (!il2cpp_init) { a2i_log_dlerror(@"dlsym il2cpp_init failed"); return; }
-        NSLog(@"[a2i] il2cpp_init=%p", il2cpp_init);
+        a2i_status([NSString stringWithFormat:@"il2cpp_init=%p", il2cpp_init]);
         NSString *metadata = [bundle pathForResource:@"global-metadata" ofType:@"dat"];
         if (metadata) {
             NSString *metadataDir = metadata.stringByDeletingLastPathComponent;
             setenv("A2I_METADATA_DIR", metadataDir.fileSystemRepresentation, 1);
-            NSLog(@"[a2i] metadata present at %@", metadata);
+            a2i_status([NSString stringWithFormat:@"metadata present: %@", metadata.lastPathComponent]);
         } else {
-            NSLog(@"[a2i] global-metadata.dat is not embedded; init may fail");
+            a2i_status(@"global-metadata.dat missing");
         }
-        NSLog(@"[a2i] calling il2cpp_init");
+        a2i_status(@"calling il2cpp_init");
         il2cpp_init("a2i-ios-host");
-        NSLog(@"[a2i] il2cpp_init returned");
+        a2i_status(@"il2cpp_init returned");
         il2cpp_shutdown_fn shutdown = (il2cpp_shutdown_fn)dlsym(il2cpp, "il2cpp_shutdown");
         if (shutdown) { NSLog(@"[a2i] calling il2cpp_shutdown"); shutdown(); }
         dlclose(il2cpp);
         dlclose(shim);
-        NSLog(@"[a2i] probe complete");
+        a2i_status(@"probe complete");
     }
 }
 
@@ -61,6 +71,8 @@ static void a2i_probe(void) {
     UIViewController *vc = [UIViewController new];
     vc.view.backgroundColor = UIColor.whiteColor;
     UILabel *label = [[UILabel alloc] initWithFrame:CGRectZero];
+    gStatusLabel = label;
+    label.font = [UIFont monospacedSystemFontOfSize:13 weight:UIFontWeightRegular];
     label.text = @"A2I IL2CPP probe running…";
     label.textAlignment = NSTextAlignmentCenter;
     label.numberOfLines = 0;
